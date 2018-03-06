966113862ccbb5cbedf4b6ee4c622866a17c38db
htharker42 <htharker@gmail.com>
2018-03-06 14:41:21 -0800

    Saved point before attempting to eff with variables for character creation

--- db/schema.rb
+++ db/schema.rb
@@ -18,10 +18,6 @@ ActiveRecord::Schema.define(version: 20180305075302) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
-    t.string "avatar_file_name"
-    t.string "avatar_content_type"
-    t.integer "avatar_file_size"
-    t.datetime "avatar_updated_at"
     t.string "image_file_name"
     t.string "image_content_type"
     t.integer "image_file_size"
