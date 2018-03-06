86f4944af19a18687d5f4f2c04935b9407fd6e5a
htharker42 <htharker@gmail.com>
2018-03-05 17:29:37 -0800

    added formatting, fixed some links - update character has stopped working

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,18 +1,23 @@
 class CharactersController < ApplicationController
+#  before_action :authenticate_user!
   before_action :find_character, only: [:show, :update, :destroy, :edit]
   def index
-
-    @character = current_user.characters.all.order("created_at DESC")
-    @user = current_user
+    if user_signed_in?
+      @character = current_user.characters.all.order("created_at DESC")
+      @user = current_user
+    else
+      redirect_to new_user_registration_path
+    end
   end
 
   def new
     @character = current_user.characters.build
+    @user = current_user
   end
 
   def create
     @character = current_user.characters.build(character_params)
-
+    @user = current_user
     if @character.save
       redirect_to @character, notice: "Save Successful"
     else
@@ -21,13 +26,15 @@ class CharactersController < ApplicationController
   end
 
   def show
-
+    @user = current_user
   end
 
   def edit
+    @user = current_user
   end
 
   def update
+    @user = current_user
     if @character.update(character_params)
       redirect_to @character, notice: "Character has been updated!"
     else
